df5208c6079d2a318e6a8ffe61fb409854b877ca
Daniel <yountdaniel@gmail.com>
2018-02-10 15:14:51 -0500

    Product page is not complete.

--- src/java/product.java
+++ src/java/product.java
@@ -38,7 +38,7 @@ public class product extends HttpServlet {
             out.println("<title>Servlet product</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet product at " + request.getContextPath() + "</h1>");
+            out.println(content);
             out.println("</body>");
             out.println("</html>");
         }
@@ -83,4 +83,33 @@ public class product extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    String content = new String("<p>User <a href=\"index\">Logout</a></p>\n" +
+"    \n" +
+"    <h1>Product</h1>\n" +
+"    <div>\n" +
+"        \n" +
+"        <form>\n" +
+"            <label>Code:</label>\n" +
+"            <input type=\"text\" name=\"code\">\n" +
+"            \n" +
+"            <br>\n" +
+"            <br>\n" +
+"            \n" +
+"            <label>Description: </label>\n" +
+"            <textarea name=\"description\"></textarea>\n" +
+"            \n" +
+"            <br>\n" +
+"            <br>\n" +
+"            \n" +
+"            <label>Price: </label>\n" +
+"            <input type=\"text\" name=\"numbers\">\n" +
+"            \n" +
+"        </form>\n" +
+"        \n" +
+"    </div>\n" +
+"    <br>\n" +
+"    <button>Update Product</button>\n" +
+"    <a href=\"products\"><button>View Products</button></a>\n" +
+"    ");
+    
 }
